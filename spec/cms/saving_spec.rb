fd1aa275bd375aab8c8987765d3147667bc306cb
Sam Taylor <sjltaylor@gmail.com>
2014-04-21 16:17:13 +0100

    display the save prompt when a stranger returns to their unsaved collection

--- spec/cms/saving_spec.rb
+++ spec/cms/saving_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper'
 
 feature 'saving' do
   background { cms.new_collection }
+
+  scenario 'displaying the save prompt' do
+    ui.cms.should_not have_save_prompt
+    cms.upload_photo
+    ui.cms.should have_save_prompt
+
+    reload
+
+    ui.cms.should have_save_prompt
+  end
+
   scenario 'a stranger creates and saves a collection' do
     ui.cms.should_not have_save_prompt
     cms.upload_photo
